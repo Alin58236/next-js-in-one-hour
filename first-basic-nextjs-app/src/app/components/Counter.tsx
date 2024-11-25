3a11752b6975cccd1ad553aaa033ea87bd394ac1
Alin Stoia <alin58236@gmail.com>
2024-11-25 04:50:54 +0200

    Client auth alternative

--- first-basic-nextjs-app/src/app/components/Counter.tsx
+++ first-basic-nextjs-app/src/app/components/Counter.tsx
@@ -2,16 +2,28 @@
 import React from 'react'
 import { useState } from 'react'
 
+import { useAuth, useUser } from '@clerk/nextjs'
+
 const Counter = () => {
+
+
+    //const { isLoaded, userId, sessionId, getToken } = useAuth(); // this is suitable for auth on client rendered pages
+
+    const {isLoaded, isSignedIn, user} = useUser(); // alternative
+    
 
     console.log("Counter Component");
 
     const [count,setCount] = useState(0);
 
+    //this must be done after invoking usestate
+    if(!isLoaded || !isSignedIn) {
+        return null;
+    }
 
     return (
 
-        <button onClick={
+        <button className='bg-vmsgreen p-4 rounded' onClick={
             
             () => setCount(count+1)
         }>
