f2c733f2d7fb7bf9135f08f67596c0ee4a87527d
Alin Stoia <alin58236@gmail.com>
2024-11-25 04:13:54 +0200

    Added simple auth and user state

--- first-basic-nextjs-app/src/app/components/Navigation.tsx
+++ first-basic-nextjs-app/src/app/components/Navigation.tsx
@@ -2,6 +2,7 @@
 import React from 'react'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
+import { SignInButton, UserButton } from '@clerk/nextjs'
 
 const Navigation = () => {
 
@@ -12,6 +13,8 @@ const Navigation = () => {
             <Link href="/" className={pathName === '/' ? 'text-green font-medium mr-4':'text-slate-200 mr-4'}>Home</Link>
             <Link href="/about" className={pathName === '/about' ? 'text-green font-medium mr-4':'text-slate-200 mr-4'}>About</Link>
             <Link href="/product/1" className={pathName.startsWith('/product/') ? 'text-green font-medium mr-4':'text-slate-200 mr-4'}>FirstProd</Link>
+            <SignInButton mode='modal'/>
+            <UserButton/>
         </nav>
 
     )
--- first-basic-nextjs-app/src/app/layout.tsx
+++ first-basic-nextjs-app/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import Navigation from "./components/Navigation";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -25,21 +26,22 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+    <ClerkProvider>
+      <html lang="en">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          <header className="bg-zinc-950 text-green p-4 text-center">
+            <Navigation />
+          </header>
 
-        <header className="bg-zinc-950 text-green p-4 text-center">
-          <Navigation/>
-        </header>
+          {children}
 
-        {children}
-
-        <footer className="bg-zinc-950 text-green p-4 text-center">
-          <p>Layout footer </p>
-        </footer>
-      </body>
-    </html>
+          <footer className="bg-zinc-950 text-green p-4 text-center">
+            <p>Layout footer </p>
+          </footer>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
